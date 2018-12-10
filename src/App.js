145b926bf2c53faae5bff8dc0f1ae9c1ddefeb1f
unknown <mbarekyasmine2017@gmail.com>
2018-12-10 07:11:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import NameFilter from "./nameFilter.js";
+import RatingFilter from "./RatingFilter.js";
+import MovieList from "./MovieList.js";
+
+
+const theMoviesList = [
+  {
+    id: 1,
+    title: "Ralph Breaks the Internet",
+    img:
+      "https://resizing.flixster.com/IaXbRF4gIPh9jireK_4VCPNfdKc=/300x0/v2/https://flxt.tmsimg.com/assets/p13044225_p_v11_ad.jpg",
+    year: "2018",
+    rate: "5"
+  },
+  {
+    id: 2,
+    title: "Dr. Seuss' The Grinch",
+    img:
+      "https://resizing.flixster.com/IaXbRF4gIPh9jireK_4VCPNfdKc=/300x0/v2/https://flxt.tmsimg.com/assets/p12806157_p_v13_ac.jpg",
+    year: "2017",
+    rate: "4"
+  },
+  {
+    id: 3,
+    title: "Creed II    ",
+    img:
+      "https://resizing.flixster.com/IaXbRF4gIPh9jireK_4VCPNfdKc=/300x0/v2/https://flxt.tmsimg.com/assets/p14810963_p_v13_ab.jpg",
+    year: "2019",
+    rate: "3"
+  },
+  
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      minrating: 1,
+      movies: theMoviesList,
+      titleFilter: ""
+    };
+    
+  }
+
+  addNewMovie(newMovie) {
+    this.setState({ movie: this.state.movies.concat(newMovie) });
+  }
+  moviesFiltred() {
+    return this.state.movies.filter(
+      el =>
+        el.rate >= this.state.minrating &&
+        el.title.toLowerCase().includes(this.state.titleFilter.trim())
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="movie-app-header">
+          <NameFilter
+            value={this.state.titleFilter}
+            onChange={newTitleFilter => {
+              this.setState({
+                titleFilter: newTitleFilter
+              });
+            }}
+          />
+          <RatingFilter
+            count={this.state.minrating}
+            onChange={newRating => {
+              this.setState({
+                minrating: newRating
+              });
+            }}
+          />
         </header>
+        <main className="movie-app-list">
+          <MovieList movielist={this.moviesFiltred()
+          }
+          onAddMovie={(newMovie) => this.addNewMovie(newMovie)} /> 
+        </main>
       </div>
     );
   }
---
+++ src/MovieList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import MovieCard from "./MovieCard";
+const MovieList = ({ movielist= [], onAddMovie = () => {}}) =>
+
+  <div className='mvlist'>
+  {
+    movielist.map((el, i) => <MovieCard card={el} key={i} />)
+  }
+
+   <div className="new-movie-card"  onClick={() => {
+                onAddMovie({
+                    id: Math.random(),
+                    title: prompt('movie title: '),
+                    rating: Number(prompt('movie rating: ')),
+                    year: Number(prompt('movie year: '))
+                })
+            }} >
+            +
+      </div>
+  </div>
+export default MovieList;
